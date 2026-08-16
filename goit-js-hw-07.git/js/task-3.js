e4a7120e345fd6b8c544835e035c53e2e2d01932
SheAsur <jermuk@gmail.com>
2026-08-16 21:00:36 +0300

    Delete goit-js-hw-07.git directory

--- goit-js-hw-07.git/js/task-3.js
+++
@@ -1,8 +0,0 @@
-const inputEl = document.querySelector('#name-input');
-const outputEl = document.querySelector('#name-output');
-
-inputEl.addEventListener('input', event => {
-  const value = event.currentTarget.value.trim();
-
-  outputEl.textContent = value === '' ? 'Anonymous' : value;
-});
